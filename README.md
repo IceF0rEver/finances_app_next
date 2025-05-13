33cd93450393e0a8fd126c5e6bdb9f422c985b5a
IceF0rEver <arnaudorban.pro@gmail.com>
2025-05-14 01:13:06 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# finances_app_next
---
+++ finances_app/src/lib/auth.ts
@@ -0,0 +1,36 @@
+import { betterAuth } from "better-auth";
+import { nextCookies } from "better-auth/next-js";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { PrismaClient } from "@/src/generated/prisma";
+// import { resend } from "./resend";
+
+ 
+const prisma = new PrismaClient();
+ 
+export const auth = betterAuth({
+	database: prismaAdapter(prisma, {
+		provider: "postgresql",
+	}),
+   	emailAndPassword: {
+        enabled: true,
+        // async sendResetPassword(data) {
+		// 	await resend.emails.send({
+		// 		from: "noreplay@finance_app.app",
+		// 		to: data.user.email,
+		// 		subject: "Reset Password",
+		// 		text: `Reset password : ${data.url}`,
+		// 	});
+        // },
+    },
+    // socialProviders: {
+    //     google: {
+    //         clientId: process.env.GOOGLE_CLIENT_ID!,
+    //         clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+    //     },
+    //     github: {
+    //         clientId: process.env.GITHUB_CLIENT_ID!,
+    //         clientSecret: process.env.GITHUB_CLIENT_SECRET!
+    //     }
+    // },
+    plugins: [nextCookies()]
+});
