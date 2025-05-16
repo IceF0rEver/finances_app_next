353ef085e886362d086574af2fe7cc9a5f6538a7
IceF0rEver <arnaudorban.pro@gmail.com>
2025-05-16 15:52:35 +0200

    feat: dashboard page

--- finances_app/src/app/dashboard/page.tsx
+++ finances_app/src/app/dashboard/page.tsx
@@ -1,5 +1,13 @@
-export default function Page() {
+import { getUser } from "@/src/lib/server";
+
+export default async function Page() {
+
+    const user = await getUser();
+
     return (
-        <div>Dashboard</div>
+        <div className="flex flex-col justify-center items-center gap-4">
+            <h1>Bienvenu sur votre dashboard !</h1>
+            <p>{user?.name}</p>
+        </div>
     )
 }
