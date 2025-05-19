e778a347035e8515e1ea06e1b312f7d0001b6978
IceF0rEver <arnaudorban.pro@gmail.com>
2025-05-19 18:54:35 +0200

    feat: change lang

---
+++ finances_app/src/components/utils/select-lang.tsx
@@ -0,0 +1,28 @@
+"use client"
+import { useI18n } from "@/src/locales/client"
+import { Select, SelectTrigger, SelectContent, SelectGroup, SelectItem, SelectValue } from "../ui/select"
+import { useChangeLocale, useCurrentLocale } from "@/src/locales/client";
+
+export default function SelectLang(){
+    const t = useI18n();
+    const changeLocale = useChangeLocale();
+    const currentLocale = useCurrentLocale();
+    
+    return (
+        <Select onValueChange={(value) => changeLocale(value as 'fr' | 'en')} value={currentLocale}>
+            <SelectTrigger>
+                <SelectValue placeholder={t('components.utils.selectLang.select')}/>
+            </SelectTrigger>
+            <SelectContent>
+                <SelectGroup>
+                    <SelectItem value="fr">
+                        { t('components.utils.selectLang.fr') }
+                    </SelectItem>
+                    <SelectItem value="en">
+                        { t('components.utils.selectLang.en') }
+                    </SelectItem>
+                </SelectGroup>
+            </SelectContent>
+        </Select>
+    )
+}
