1b1d7ec6f6fbec275b6fca549ac51042cc044f94
IceF0rEver <arnaudorban.pro@gmail.com>
2025-06-25 14:50:15 +0200

    refact: setting item selected

--- finances_app/src/app/[locale]/dashboard/settings/page.tsx
+++ finances_app/src/app/[locale]/dashboard/settings/page.tsx
@@ -11,16 +11,19 @@ import { useState } from "react";
 import { useI18n } from "@/locales/client";
 import Appearance from "@/components/settings/appearance";
 import Account from "@/components/settings/account";
+import { useRouter, useSearchParams } from "next/navigation";
 
 export default function Page() {
 	const t = useI18n();
-	const [settingItemSelected, setSettingItemSelected] = useState<string>("appearance");
-
+	const router = useRouter();
+	const searchParams = useSearchParams();
+	const [settingItemSelected, setSettingItemSelected] = useState<string>(searchParams.get('selected') || "appearance");
+	
 	const items = [
 		{ title: t("app.dashboard.settings.page.items.appearance.title"), key: "appearance" },
 		{ title: t("app.dashboard.settings.page.items.account.title"), key: "account" },
 	];
-
+	
 	return (
 		<section className="md:grid md:grid-cols-5 md:p-6">
 			<aside className="md:col-span-1">
@@ -34,7 +37,12 @@ export default function Page() {
 											asChild
 											variant={"primary"}
 											isActive={settingItemSelected === item.key}
-											onClick={() => setSettingItemSelected(item.key)}
+											onClick={() => {
+												setSettingItemSelected(item.key);
+												const params = new URLSearchParams(searchParams);
+												params.set('selected', item.key);
+												router.replace(`?${params.toString()}`);
+											}}
 										>
 											<span>{item.title}</span>
 										</SidebarMenuButton>
