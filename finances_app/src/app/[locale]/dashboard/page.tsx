e262abe8ad91d6f957809fe1f13b49e2af685516
IceF0rEver <arnaudorban.pro@gmail.com>
2025-10-03 20:30:28 +0200

    fix: dashboard page

--- finances_app/src/app/[locale]/dashboard/page.tsx
+++ finances_app/src/app/[locale]/dashboard/page.tsx
@@ -1,9 +1,7 @@
 import { getUser } from "@/lib/auth/server";
-import { getI18n } from "@/locales/server";
 
 export default async function Page() {
 	const user = await getUser();
-	const t = await getI18n();
 
 	return (
 		<section className="flex flex-col justify-center items-center gap-4">
