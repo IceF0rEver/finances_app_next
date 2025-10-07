d9ff579e6ac571e710a04f864a6b8b40d072288a
IceF0rEver <arnaudorban.pro@gmail.com>
2025-10-07 17:54:37 +0200

    refactor: subscription schema

--- finances_app/src/lib/zod/subscription-schemas.ts
+++ finances_app/src/lib/zod/subscription-schemas.ts
@@ -1,21 +1,30 @@
 import { z } from "zod";
 import type { useI18n } from "@/locales/client";
+
+export const subscriptionTableSchema = z.object({
+	id: z.string().uuid().optional(),
+	name: z.string(),
+	amount: z.coerce.number(),
+	recurrence: z.string(),
+	executionDate: z.union([z.date(), z.string()]),
+	icon: z.string(),
+	userId: z.string().uuid(),
+});
+
 export const subscriptionSchemas = (t: ReturnType<typeof useI18n>) => ({
 	subscription: z.object({
 		id: z.string().optional(),
-		name: z.string().min(1, { message: t("action.subscription.form.name") }),
-		amount: z.coerce
-			.number()
-			.min(0, { message: t("action.subscription.form.amount") }),
-		recurrence: z.string({ message: t("action.subscription.form.recurrence") }),
-		executionDate: z.union([
-			z.date({ message: t("action.subscription.form.executionDate") }),
-			z.string({ message: t("action.subscription.form.executionDate") }),
-		]),
+		name: subscriptionTableSchema.shape.name.min(1, {
+			message: t("action.subscription.form.name"),
+		}),
+		amount: subscriptionTableSchema.shape.amount.min(0, {
+			message: t("action.subscription.form.amount"),
+		}),
+		recurrence: z.string({
+			message: t("action.subscription.form.recurrence"),
+		}),
+		executionDate: subscriptionTableSchema.shape.executionDate,
 		icon: z.string({ message: t("action.subscription.form.icon") }),
-		userId: z.string(),
-	}),
-	deleteSubscription: z.object({
-		id: z.string().min(1),
+		userId: subscriptionTableSchema.shape.userId,
 	}),
 });
